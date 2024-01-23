4f6c44b3cb931da8558b99c0f4b6774e4b14cb0b
Dustin <dustin04ms@gmail.com>
2024-01-22 20:42:14 -0500

    Segundo Comit

--- Exposicion/src/form1.java
+++ Exposicion/src/form1.java
@@ -1,6 +1,18 @@
 import javax.swing.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class form1 {
     JPanel Programa;
-    private JTable Table;
+    private JButton visualizarNotasButton;
+    private JButton explorarArchivosButton;
+    private JTable table1;
+
+    public form1() {
+
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
